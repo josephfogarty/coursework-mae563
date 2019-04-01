ebafe3a8f5dec35b67a799398e21b9a8a2f809bc
Joseph Fogarty <36773696+jjf1218@users.noreply.github.com>
2019-03-31 22:01:47 -0400

    updates pset2

--- Kelvin - Helmholtz Instability/pset2_energy.m
+++ Kelvin - Helmholtz Instability/pset2_energy.m
@@ -1,13 +1,14 @@
-%% A short code to retrieve both interface and energy data from the Basilisk
-% Kelvin-Helmholtz solver and plot them
+%% A short code to retrieve both interface and energy data
+% from the Basilisk Kelvin-Helmholtz solver and plot them
 clear all;
 clc;
 clf;
+cd 'D:\Coursework\MAE 563 Instabilities in Fluids\MAE563\Kelvin - Helmholtz Instability'
 
 %% Plotting Energy %%
 
 % Import simulation results
-energy_results = importdata('kh_vortex_sheet/u2_gt_vc/new/budgetFluid1.dat');
+energy_results = importdata('kh_vortex_sheet/pwp/budgetFluid1.dat');
 energy_results = energy_results.data(2:end,1:2);
 %[m1,n1] = size(energy_results);
 %fprintf("The size of the raw data is %d by %d\n", m1, n1);
@@ -17,17 +18,17 @@ x2 = energy_results(:,1);
 y2 = energy_results(:,2);
 
 % Now plot energy
-scatter(x2,y2,7,'filled','k')
+scatter(x2,y2,7,'filled','b')
 % plot(x,y)
 ylabel('Energy (E)');
 xlabel('time (t)');
-titlestr = sprintf("Energy for 0.02 thickness BL");
+titlestr = sprintf("Energy for Perturbation without Propagation");
 title(titlestr);
 grid on
 
 % Optional include legend
 %legend('\gamma = 0.1, g = 10','\gamma = 0.05, g = 10','\gamma = 0.05, g = 12')
 
-%% Growth Rate
-xgrowth=x2(1:161);
-ygrowth=y2(1:161);
+% Growth Rate
+xgrowth=x2(1:257);
+ygrowth=y2(1:257);
--- Kelvin - Helmholtz Instability/pset2_interfaces.m
+++ Kelvin - Helmholtz Instability/pset2_interfaces.m
@@ -3,12 +3,13 @@
 clear all;
 clc;
 clf;
+cd 'D:\Coursework\MAE 563 Instabilities in Fluids\MAE563\Kelvin - Helmholtz Instability'
 
 %% Plotting Interface Data %%%
 
 % Import simulation results, and change the file, assuming you are
 % already in MAE563/Rayleigh - Taylor Instability
-interface_results = importdata('kh_vortex_sheet/u2_gt_vc/results0.20_0.dat');
+interface_results = importdata('kh_vortex_sheet/pwp/results0.50_0.dat');
 % [m1,n1] = size(interface_results);
 % fprintf("The size of the raw data is %d by %d\n", m1, n1);
 
@@ -16,11 +17,11 @@ x1 = interface_results(:,1);
 y1 = interface_results(:,2);
 
 % Set Up the Figure
-scatter(x1,y1,7,'filled','k');
+scatter(x1,y1,7,'filled','m');
 
 % Add legend, axes, grid, and title
 grid on
 xlabel('x');
 ylabel('z');
-%legend('t = 0.0','t = 0.2','t = 0.4','t = 0.8')%,'t = 2.0')
-title("Interfaces for KH Instability, above V_c");
+legend('t = 0.0','t = 0.1','t = 0.2','t = 0.3','t = 0.5')
+title("Interfaces for KH Instability, Perturbation without Propagation");
